b8cd6533d32997789227669ae11de47de165b26b
shushantsood09 <shushantsood89012@gmil.com>
2022-04-25 22:24:11 +0530

    add two numbers

---
+++ Basics/add-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter first number : " << endl;
+    cin >> a;
+    cout << "Enter second number : " << endl;
+    cin >> b;
+    int sum = a + b;
+    cout << "Sum is : " << sum;
+    return 0;
+}

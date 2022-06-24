ba8fe25a6b5a975a1afef012ee47ac97a2cdf5bc
shushantsood09 <shushantsood89012@gmail.com>
2022-06-24 23:16:23 +0530

    arrays and functions

---
+++ Basics/Arrays-basics/array-basics.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Array Creation
+    int arr[10];
+
+    //Second way 
+    int arr1[5]= {1,2,3,4,5};
+
+    // Third way
+    int arr2[]= {1,2,3,4,5};
+
+    //Fourth Way
+    int arr3[15] = {1,2};
+    
+    // Lets say initialize a array with a single value.
+
+    int arr4[200]={0};
+
+    return 0;
+}
